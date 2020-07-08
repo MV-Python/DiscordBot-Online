e91ccaf8a7cea9061210a42357bac8f5c7436ae2
MV_ <mvalentin838@gmail.com>
2020-07-08 22:52:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+'''
+To run the code on your bot, create file named exactly ".env"
+in the Repl.it files and type "TOKEN="[Your Token]
+
+Your token will be hidden from everyone except you!
+'''
+import DiscordBot
