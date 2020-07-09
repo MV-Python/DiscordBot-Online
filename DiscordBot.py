059bce23954a04d839c4c8daaca7be1b0be3c56c
MV-Python <46030721+MV-Python@users.noreply.github.com>
2020-07-08 22:45:12 -0400

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -5,20 +5,15 @@ import asyncio
 import discord
 import time
 #---Client Commands---
-def createBot(token, email=None, password=None, bot=True):
-    asyncio.set_event_loop(asyncio.new_event_loop())
-    loop = asyncio.get_event_loop()
-    client = discord.Client()
-    loop.create_task(client.start(token, bot=bot))
-    return client
-def run():
-    asyncio.get_event_loop().run_forever()
+client = discord.Client()
+def restart():
+    os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
 class clientCommands():
     def __init__(self, client):
         self.client = client
-        asyncio.get_event_loop().create_task(client.wait_until_ready())
-    def restart():
-        os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
+
+    def task1(self):
+        print("hi")
 
 TOKEN = input("Token > ")
 TOKEN = TOKEN.strip('''"''')
@@ -27,10 +22,11 @@ if BOT in ["true", "True", "t", "T"]:
     BOT = True
 if BOT in ["false", "False", "f", "F"]:
     BOT = False
-bot = clientCommands(client = createBot(TOKEN, bot=BOT))
+bot = clientCommands(client = client)
+
 @bot.client.event
 async def on_ready():
-    print('ready')
-    time.sleep(10)
+    bot.task1()
+
 print("loading...")
-run()
+bot.client.run(TOKEN, bot=BOT)
