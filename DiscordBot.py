9d73211eb37aea8359b36ec91fa0f20fd9690fe6
MV-Python <46030721+MV-Python@users.noreply.github.com>
2020-07-08 21:40:23 -0400

    making template

--- DiscordBot.py
+++ DiscordBot.py
@@ -3,31 +3,34 @@
 #---Imports---
 import asyncio
 import discord
-
+import time
 #---Client Commands---
+def createBot(token, email=None, password=None, bot=True):
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    loop = asyncio.get_event_loop()
+    client = discord.Client()
+    loop.create_task(client.start(token, bot=bot))
+    return client
+def run():
+    asyncio.get_event_loop().run_forever()
 class clientCommands():
-    def _init_:
+    def __init__(self, client):
         self.client = client
         asyncio.get_event_loop().create_task(client.wait_until_ready())
-    def createBot(token, email=None, password=None, bot=True):
-        asyncio.set_event_loop(asyncio.new_event_loop())
-        loop = asyncio.get_event_loop()
-        client = discord.Client()
-        loop.create_task(client.start(token, bot=bot))
-        return client
-    def run():
-        asyncio.get_event_loop().run_forever()
     def restart():
         os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
 
-bot = cleintCommands(self)
 TOKEN = input("Token > ")
+TOKEN = TOKEN.strip('''"''')
 BOT = input("Bot T/F > ")
 if BOT in ["true", "True", "t", "T"]:
     BOT = True
 if BOT in ["false", "False", "f", "F"]:
     BOT = False
-client = bot.createBot(TOKEN, bot=BOT)
-@cleint.event
+bot = clientCommands(client = createBot(TOKEN, bot=BOT))
+@bot.client.event
 async def on_ready():
     print('ready')
+    time.sleep(10)
+print("loading...")
+run()
