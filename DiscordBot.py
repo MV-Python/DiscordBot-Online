7464b765a7e7660220b655fd52bcebeffccc3696
MV-Python <46030721+MV-Python@users.noreply.github.com>
2020-07-08 21:16:01 -0400

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,13 +1,33 @@
 #rewrite
 
-#>Imports
-import async
+#---Imports---
+import asyncio
 import discord
 
-#>Client Commands
+#---Client Commands---
 class clientCommands():
     def _init_:
         self.client = client
         asyncio.get_event_loop().create_task(client.wait_until_ready())
-    def keep_alive():
-        keep_alive()
+    def createBot(token, email=None, password=None, bot=True):
+        asyncio.set_event_loop(asyncio.new_event_loop())
+        loop = asyncio.get_event_loop()
+        client = discord.Client()
+        loop.create_task(client.start(token, bot=bot))
+        return client
+    def run():
+        asyncio.get_event_loop().run_forever()
+    def restart():
+        os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
+
+bot = cleintCommands(self)
+TOKEN = input("Token > ")
+BOT = input("Bot T/F > ")
+if BOT in ["true", "True", "t", "T"]:
+    BOT = True
+if BOT in ["false", "False", "f", "F"]:
+    BOT = False
+client = bot.createBot(TOKEN, bot=BOT)
+@cleint.event
+async def on_ready():
+    print('ready')
