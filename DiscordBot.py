df6c80c2f00b52f358023536e367bc2a632b206e
MV-Python <46030721+MV-Python@users.noreply.github.com>
2020-07-08 20:54:43 -0400

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -9,3 +9,5 @@ class clientCommands():
     def _init_:
         self.client = client
         asyncio.get_event_loop().create_task(client.wait_until_ready())
+    def keep_alive():
+        keep_alive()
