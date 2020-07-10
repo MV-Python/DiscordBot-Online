673b67722f500c2cc2f53524aa3497dd82a1d92d
MV-Python <46030721+MV-Python@users.noreply.github.com>
2020-07-10 02:05:28 -0400

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -20,24 +20,30 @@ async def controlPanel(self):
     loop = True
     while loop == True:
         choice = input(" >>> ")
+        if choice == "help":
+            print("Command List\n------------")
+            for command in dir(clientCommands):
+                if not command.startswith("__"):
+                    if command != "help":
+                        print(command + str(inspect.signature(getattr(clientCommands, command))).replace("self, ", "").replace("self", ""))
         if choice.split("(", 1)[0] in dir(clientCommands):
             if "(" in choice:
                 func = getattr(clientCommands, choice.split("(",1)[0])
                 execPrefix = ""
                 if inspect.iscoroutinefunction(func):
                     execPrefix = "await "
-                print("Running Command: " + choice)
-                loop = False
-                print(("async def mainCode(self):\n\t" + execPrefix + "self." + choice.replace("self", "").replace("self, ", "")))
-                exec("async def mainCode(self):\n\t" + execPrefix + "self." + choice.replace("self", "").replace("self, ", ""), globals())
+                print("Running Command: " + choice + "...\n")
+                exec("async def mainCode(self):\n\t" + execPrefix + "self." + choice.replace("self, ", "").replace("self", ""), globals())
                 await mainCode(self)
+                loop = False
 #---Client Commands---
 class clientCommands():
     def __init__(self, client):
         self.client = client
-    async def sendMessage(self, channelID):
+    async def sendMessage(self, channelID, message):
         channel = self.client.get_channel(int(channelID))
-        await channel.send("hello")
+        await channel.send(message)
+        print("Message sent")
     def autoListen(self):
         @self.client.event
         async def on_message(message):
@@ -48,11 +54,12 @@ bot = clientCommands(client = client)
 @bot.client.event
 async def on_ready():
     t1 = time.time()
-    print("\n------\n")
+    print("\n------")
     print("Logged Into: " + str(client.user.name))
     print("Login Time: " + str(round(t1-t0, 1)) + "s")
-    print("\n------\n")
-    await controlPanel(bot)
+    print("------\n")
+    while True:
+        await controlPanel(bot)
 #----Live Loop---
 TOKEN = input("Token   > ")
 TOKEN = TOKEN.strip('''"''')
